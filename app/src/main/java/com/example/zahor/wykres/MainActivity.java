c9e56489089a632acc6faa7af09984130d4f8ad5
zahor16 <zahor16@gmail.com>
2015-12-17 17:30:02 +0100

    1

---
+++ app/src/main/java/com/example/zahor/wykres/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.zahor.wykres;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private RelativeLayout mainLayout;
+    private PieChart mChart;
+
+    private float [] yData = {5,10,15,30,25};
+    private String[] xData = {"Sony","Huawei","LG","Apple","Samsung"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainLayout =(RelativeLayout)findViewById(R.id.mainLayout);
+        mChart = new PieChart(this);
+        mainLayout.addView(mChart);
+        mainLayout.setBackgroundColor(Color.LTGRAY);
+
+        mChart.setUsePercentValues(true);
+        mChart.setDescription("Smatphones");
+
+        mChart.setDrawHoleEnabled(true);
+        mChart.setHoleColorTransparent(true);
+        mChart.setHoleRadius(7);
+        mChart.setTransparentCircleRadius(10);
+
+        mChart.setRotationAngle(0);
+        mChart.setRotationEnabled(true);
+
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, int i, Highlight highlight) {
+                if(e==null)
+                    return;
+
+                Toast.makeText(MainActivity.this, xData[e.getXIndex()]+"="+e.getVal()+"%",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+        addData();
+
+        Legend l=mChart.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
+
+
+    }
+
+    private void addData() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for(int i=0;i<yData.length;i++)
+            yVals1.add(new Entry(yData[i],i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for(int i=0; i<xData.length; i++)
+            xVals.add(xData[i]);
+
+        PieDataSet dataSet = new PieDataSet(yVals1, "Market Share");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for(int c: ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for(int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for(int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for(int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for(int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        mChart.setData(data);
+        mChart.highlightValues(null);
+        mChart.invalidate();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
